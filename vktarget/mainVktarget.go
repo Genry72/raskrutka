735be45dede3dc0c7c714508fc503bf45b23c66f
Valentin Semenov <valentin.semyonov@nexign.com>
2022-01-06 15:31:58 +0300

    Завернул в циклы

--- vktarget/mainVktarget.go
+++ vktarget/mainVktarget.go
@@ -3,6 +3,7 @@ package vktarget
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"raskrutka/all"
 	"raskrutka/vk"
@@ -23,12 +24,6 @@ func MainVktarget(loginVK, passVK, versionApiVK string) {
 		errorLog.Printf("ошибка получения sessID: %v", err)
 		return
 	}
-	//Делаем куку рабочей
-	err = all.GetUloginToken(loginVK, passVK, sessID, site)
-	if err != nil {
-		errorLog.Printf("ошибка получения UloginToken %v", err)
-		return
-	}
 	//Получаем токен пользователя
 	tokenVK, _, err := vk.GetToken(loginVK, passVK)
 	if err != nil {
@@ -36,46 +31,74 @@ func MainVktarget(loginVK, passVK, versionApiVK string) {
 		errorLog.Println(err)
 		return
 	}
-	//Получаем задания
-	jobList, err := GetDjob(loginVK, passVK, sessID)
+	//Делаем куку рабочей
+	err = all.GetUloginToken(loginVK, passVK, sessID, site)
 	if err != nil {
-		errorLog.Printf("Ошибка получения информации по заданиям vktarget: %v", err)
+		errorLog.Printf("ошибка получения UloginToken %v", err)
 		return
 	}
-	time.Sleep(5 * time.Second)
-	for jobID, value := range jobList {
-		switch value[0] { //Тип заданимя
-		case "Вступите в сообщество":
-			groupName := strings.Split(value[1], "/") //бьем слешами адрес группы для получения имени
-			//Получаем id группы
-			groupID, _, err := vk.GroupsGetById(groupName[1], tokenVK, versionApiVK)
+	//Раз в 30 минут продлеваем куку
+	go func() {
+		for {
+			time.Sleep(30 * time.Minute)
+			//Делаем куку рабочей
+			err = all.GetUloginToken(loginVK, passVK, sessID, site)
 			if err != nil {
-				err = fmt.Errorf("ошибка получения id группы %v %v", groupName[1], err)
-				errorLog.Println(err)
-				continue
+				errorLog.Printf("ошибка получения UloginToken %v", err)
+				return
 			}
-			//Вступаем в группу
-			err = vk.GroupsJoin(versionApiVK, tokenVK, groupID)
-			if err != nil {
-				err = fmt.Errorf("ошибка вступления в группу %v пользователем %v %v", groupID, loginVK, err)
-				errorLog.Println(err)
-			}
-			time.Sleep(3 * time.Second)
-			//Проверяем задание
-			err = CheckJob(jobID, sessID)
-			if err != nil {
-				err = fmt.Errorf("ошибка проверки задания %v", jobID)
-				errorLog.Println(err)
-			}
-		case "Нажмите поделиться записью":
-			infoLog.Println("Репост")
-		case "Поставьте лайк на странице":
-			infoLog.Println("Лайк")
-		case "Расскажите о группе":
-			infoLog.Println("Рассказать о группе")
-		default:
-			err = fmt.Errorf("не известный тип задания %v", value[0])
-			errorLog.Println(err)
 		}
+	}()
+	//Раз в 30 секунд проверяем задания
+	for {
+		rand.Seed(time.Now().Unix())                          //Seed для рандома
+		son := time.Duration(30+rand.Intn(300)) * time.Second // Сон, рандом от 1 мин до 5 минут
+		//Получаем задания
+		jobList, err := GetDjob(loginVK, passVK, sessID)
+		if err != nil {
+			errorLog.Printf("Ошибка получения информации по заданиям vktarget: %v", err)
+			infoLog.Printf("Спим %v перед следующей проверкой задания из-за ошибки", son)
+			time.Sleep(son)
+			continue
+		}
+		time.Sleep(5 * time.Second)
+		for jobID, value := range jobList {
+			switch value[0] { //Тип заданимя
+			case "Вступите в сообщество":
+				groupName := strings.Split(value[1], "/") //бьем слешами адрес группы для получения имени
+				//Получаем id группы
+				groupID, _, err := vk.GroupsGetById(groupName[1], tokenVK, versionApiVK)
+				if err != nil {
+					err = fmt.Errorf("ошибка получения id группы %v %v", groupName[1], err)
+					errorLog.Println(err)
+					continue
+				}
+				//Вступаем в группу
+				err = vk.GroupsJoin(versionApiVK, tokenVK, groupID)
+				if err != nil {
+					err = fmt.Errorf("ошибка вступления в группу %v пользователем %v %v", groupID, loginVK, err)
+					errorLog.Println(err)
+				}
+				time.Sleep(3 * time.Second)
+				//Проверяем задание
+				err = CheckJob(jobID, sessID)
+				if err != nil {
+					err = fmt.Errorf("ошибка проверки задания %v", jobID)
+					errorLog.Println(err)
+				}
+			case "Нажмите поделиться записью":
+				infoLog.Println("Репост")
+			case "Поставьте лайк на странице":
+				infoLog.Println("Лайк")
+			case "Расскажите о группе":
+				infoLog.Println("Рассказать о группе")
+			default:
+				err = fmt.Errorf("не известный тип задания %v", value[0])
+				errorLog.Println(err)
+			}
+		}
+		infoLog.Printf("Спим %v перед следующей проверкой задания", son)
+		// infoLog.Printf("Спим 10c перед следующей проверкой задания", son)
+		time.Sleep(son)
 	}
 }
